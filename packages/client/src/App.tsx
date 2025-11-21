8205893baab7c1472da28e28b62f39170f31d1ac
Emmanuel Egomson <fegomson@gmail.com>
2025-11-21 11:11:31 -0600

    feat: enhance client application with new UI components and styling, update dependencies, and configure TypeScript paths

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { Button } from "./components/ui/button"
 
 const App = () => {
   const [message, setMessage] = useState("")
@@ -11,7 +12,12 @@ const App = () => {
   }, [])
 
   return (
-    <div className="text-4xl text-green-500">{message}</div>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <p className="text-green-600 text-center text-2xl">{message}</p>
+      <div className="mt-4">
+        <Button onClick={() => console.log("Button Clicked!")}>Click me</Button>
+      </div>
+    </div>
   )
 }
 export default App
