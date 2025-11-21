991148e9eca031b9bdc5044bcd4e7d80cdbe558a
Emmanuel Egomson <fegomson@gmail.com>
2025-11-21 10:57:44 -0600

    feat: add concurrent development setup and dotenv dependency
    - Implemented a concurrent development setup using `concurrently` to run both server and client with a single command.
    - Updated `package.json` to include workspaces and a new `dev` script for starting the application.
    - Added `dotenv` as a dependency in the server package for environment variable management.

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import concurrently from "concurrently";
+
+concurrently([
+    {
+        name: "server",
+        command: "bun dev",
+        cwd: "./packages/server",
+        prefixColor: "cyan"
+    },
+    {
+        name: "client",
+        command: "bun dev",
+        cwd: "./packages/client",
+        prefixColor: "green"
+    }
+], {
+    killOthersOn: ["failure", "success"],
+    restartTries: 3
+})
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,17 @@
 import express, { type Request, type Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5005;
 
 app.get('/alive', (req: Request, res: Response) => {
     res.send('Hello! Server is ALIVE!!!');
+});
+
+app.get("/api/status", (req: Request, res: Response) => {
+    res.json({ status: "ok", timestamp: new Date().toISOString() });
 });
 
 app.listen(PORT, () => {
